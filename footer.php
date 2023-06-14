9d7f5dfeee073ccf17eccd55bb15b4416166eb0c
sk76329 <sk7632958125@gmail.com>
2023-06-14 22:34:13 +0530

    Online Book Store

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick Links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About Us</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact Us</a>
+      </div>
+
+      <div class="box">
+         <h3>Access Link</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us</h3>
+         <p> <i class="fas fa-phone"></i> +91 1850 222 333 </p>
+         <p> <i class="fas fa-phone"></i> +91 1800 000 123 </p>
+         <p> <i class="fas fa-envelope"></i> Onlinebookstore@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Kr Puram Bangalore </p>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> Linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; Copyright @ <?php echo date('Y'); ?> by <span>Online Book Store </span> </p>
+
+</section>
